a78f5d3077081b98f2dff996695145beaffa7370
Samarth2190 <samarthruia@gmail.com>
2026-03-16 23:29:43 +0530

    chore: initialize node devopsproject skeleton

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: false,
+    es2021: true,
+    node: true,
+    mocha: true
+  },
+  extends: ['standard'],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module'
+  },
+  rules: {
+  }
+}
+
